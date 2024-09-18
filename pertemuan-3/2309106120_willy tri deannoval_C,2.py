3ba9c31414166a3392a43d81556f47a543f86b76
willytri <trideannoval@gmail.com>
2024-09-18 16:20:17 +0800

    upload code pertemuan 3

---
+++ pertemuan-3/2309106120_willy tri deannoval_C,2.py
@@ -0,0 +1,49 @@
+# cuaca = "mendung"
+
+# if cuaca == "mendung" :
+#     print("diam dirumah")
+# else :
+#     print("hari ini mendung")
+
+# umur = int(input("masuka umur anda :"))
+# if umur > 0:
+#     if umur <= 10:
+#         print("umur anda termasuk kategori anak-anak")
+#     elif umur <= 18:
+#         print("umur anda termasuk kategori remaja")
+#     elif umur <= 35:
+#         print("umur anda termasuk kategori dewasa")
+#     elif umur <= 65:
+#         print("umur anda termasuk kategori paru baya")
+#     else :
+#         print("umur anda termasuk kategoritua")
+# else : 
+#     print("input usia harus biolangan positif")
+
+# nim = int(input("masukan nim: "))
+
+# if nim >= 1 and nim <= 50:
+#     if nim >=51 and nim <= 75 :
+#         print("Kelas A'1")
+#     else :
+#         print("kelas A'2")
+# elif nim >= 51 and nim <= 100:
+#     if nim >= 51 and nim <= 75 :
+#         print("kelas B'1")
+#     else :
+#         print("kelas B'2")
+# elif nim >= 101 and nim <= 121 :
+#     if nim >= 101 and nim <= 110 :
+#         print("kelas C'1")
+#     else :
+#         print("kelas C'2")
+# else:
+#     print("anda bukan mahasiswa informatika 24")
+
+#
+# angka = int(input("masuka angka: "))
+# result = "genap" if angka % 2 == 0 else "ganjil"
+# print(result)
+# nim = int(input("masukan nim: "))
+# hasil ="kelas A" if nim >= 1 and nim <= 50 else "kelas B" if nim >= 51 and nim <= 100 else "kelas C" if nim >= 101 and nim <= 121 else "mahasiswa siluman"
+# print(hasil)
